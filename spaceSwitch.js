79c498f926bc6a198901f91fe1e167b157c2abc8
zagood <ljbfif50@gmail.com>
2020-12-28 16:42:03 +0900

    jQuery 시작 함수 수정

--- spaceSwitch.js
+++ spaceSwitch.js
@@ -1,19 +1,17 @@
-const start = function(){
+$(document).ready(function () {
 
     const hi = '연결 성공';
     console.log(hi);
-    
+
     const list = ['권시연', '김예은', '김예진', '김재영', '노유림', '민지홍', '박윤재', '이소현', '이재빈', '이지현', '임정환', '정우리'];
-    
-    $('li').each(list, function(){
-        console.log(list);
-    });
 
-    for(const listNum in list){
-
-        $('li:eq(listNum)').text(list[listNum]);
-        console.log(listNum);
-        console.log(list[listNum]);
+    const orderPick = function () {
+        for (const listNum in list) {
+            $('li').eq(listNum).text(list[listNum]);
+        }
     }
-    
-}();
+
+    $("#randomSeat").on("click", function () {
+        orderPick();
+    });
+});
